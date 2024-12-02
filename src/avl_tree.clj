f8169582cbdb456c5552e026cc438861f2ba83a3
Fedor <fifero@mail.ru>
2024-12-02 15:41:56 +0300

    changed insert, removed additional information

--- src/avl_tree.clj
+++ src/avl_tree.clj
@@ -92,18 +92,20 @@
     node
     (if (type-checking root node)
       (let [cmp (compare (:key root) (:key node))]
-        (if (or (zero? cmp) (neg? cmp))
+        (cond
+          (zero? cmp)
+          (assoc root :value (:value node))
+          (neg? cmp)
           (if (nil? (:right root))
             (assoc root :right node :height 2)
             (let [new-node (balance (insert (:right root) node))]
               (balance (assoc root :right new-node))))
+          (pos? cmp)
           (if (nil? (:left root))
             (assoc root :left node :height 2)
             (let [new-node (balance (insert (:left root) node))]
               (balance (assoc root :left new-node))))))
-      (do
-        (println "Key Error: Invalid key type")
-        root))))
+      (throw (Exception. "Key Error: Invalid key type")))))
 
 (defn search-node [node key]
   (let [cmp (compare (:key node) key)]
@@ -138,7 +140,7 @@
     (let [l (map-avl f (:left node))
           r (map-avl f (:right node))
           new-value (f [(:key node) (:value node)])]
-      (into () (concat l [new-value] r)))))
+      (into () (reverse (concat l [new-value] r))))))
 
 (defn filter-avl [f node]
   (when node
@@ -181,13 +183,10 @@
       (let [cmp (compare (:key root) (:key node))]
         (cond
           (zero? cmp)
-          (try
-            (let [new-value (merge-type (:value root) (:value node))
-                  new-node (create-node (:key node) new-value)
-                  new-tree (delete root (:key node))]
-              (insert new-tree new-node))
-            (catch Exception e
-              (throw e)))
+          (let [new-value (merge-type (:value root) (:value node))
+                new-node (create-node (:key node) new-value)
+                new-tree (delete root (:key node))]
+            (insert new-tree new-node))
           (neg? cmp)
           (if (nil? (:right root))
             (assoc root :right node :height 2)
@@ -198,9 +197,7 @@
             (assoc root :left node :height 2)
             (let [new-node (balance (merge-insert (:left root) node))]
               (balance (assoc root :left new-node))))))
-      (do
-        (println "Key Error: Invalid key type")
-        root))))
+      (throw (Exception. "Key Error: Invalid key type!!!!!!!!!!")))))
 
 (defn merge [root1 root2]
   (let [keys (map-avl first root2)
@@ -210,13 +207,9 @@
            new-tree root1]
       (if (> i (- length 1))
         new-tree
-        (recur (inc i) (try
-                         (merge-insert new-tree (create-node (nth keys i) (nth values i)))
-                         (catch Exception e
-                           (println (.getMessage e))
-                           nil)))))))
+        (recur (inc i) (merge-insert new-tree (create-node (nth keys i) (nth values i))))))))
 
-(defn visualize [node]
+(defn- visualize [node]
   (letfn [(build-tree [node prefix is-left]
             (when node
               (str
@@ -227,104 +220,3 @@
                 "\n"
                 (build-tree (:left node) (str prefix (if is-left "    " "│   ")) true))))]
     (println (build-tree node "" false))))
-
-;slight-right-rotation
-;(def o (AVLTreeVertex. "o" 1 1 nil nil))
-;(def n (AVLTreeVertex. "n" 1 2 o nil))
-;(def m (AVLTreeVertex. "m" 1 1 nil nil))
-;(def f (AVLTreeVertex. "f" 1 3 n m))
-;(def h (AVLTreeVertex. "h" 1 1 nil nil))
-;(def e (AVLTreeVertex. "e" 1 2 nil h))
-;(def d (AVLTreeVertex. "d" 1 4 f e))
-;(def j (AVLTreeVertex. "j" 1 1 nil nil))
-;(def c (AVLTreeVertex. "c" 1 1 nil nil))
-;(def b (AVLTreeVertex. "b" 1 2 j c))
-;(def a (AVLTreeVertex. "a" 1 5 d b))
-
-;slight-left-rotation
-;(def o (AVLTreeVertex. "o" 1 1 nil nil))
-;(def n (AVLTreeVertex. "n" 1 2 nil o))
-;(def m (AVLTreeVertex. "m" 1 1 nil nil))
-;(def f (AVLTreeVertex. "f" 1 3 m n))
-;(def j (AVLTreeVertex. "j" 1 1 nil nil))
-;(def h (AVLTreeVertex. "h" 1 1 nil nil))
-;(def e (AVLTreeVertex. "e" 1 2 h j))
-;(def d (AVLTreeVertex. "d" 1 4 e f))
-;(def g (AVLTreeVertex. "g" 1 1 nil nil))
-;(def c (AVLTreeVertex. "c" 1 1 nil nil))
-;(def b (AVLTreeVertex. "b" 1 2 c g))
-;(def a (AVLTreeVertex. "a" 1 5 b d))
-
-;big-right-rotation
-;(def m (AVLTreeVertex. "m" 1 1 nil nil))
-;(def l (AVLTreeVertex. "l" 1 2 nil m))
-;(def k (AVLTreeVertex. "k" 1 1 nil nil))
-;(def j (AVLTreeVertex. "j" 1 1 nil nil))
-;(def h (AVLTreeVertex. "h" 1 1 nil nil))
-;(def d (AVLTreeVertex. "d" 1 2 h j))
-;(def e (AVLTreeVertex. "e" 1 3 k l))
-;(def b (AVLTreeVertex. "b" 1 4 d e))
-;(def g (AVLTreeVertex. "g" 1 1 nil nil))
-;(def f (AVLTreeVertex. "f" 1 1 nil nil))
-;(def c (AVLTreeVertex. "c" 1 2 f g))
-;(def a (AVLTreeVertex. "a" 1 5 b c))
-
-;big-left-rotation
-;(def m (AVLTreeVertex. "m" 1 1 nil nil))
-;(def l (AVLTreeVertex. "l" 1 1 nil nil))
-;(def k (AVLTreeVertex. "k" 1 1 nil nil))
-;(def j (AVLTreeVertex. "j" 1 2 nil m))
-;(def h (AVLTreeVertex. "h" 1 1 nil nil))
-;(def g (AVLTreeVertex. "g" 1 2 k l))
-;(def f (AVLTreeVertex. "f" 1 3 h j))
-;(def c (AVLTreeVertex. "c" 1 4 f g))
-;(def e (AVLTreeVertex. "e" 1 1 nil nil))
-;(def d (AVLTreeVertex. "d" 1 1 nil nil))
-;(def b (AVLTreeVertex. "b" 1 2 d e))
-;(def a (AVLTreeVertex. "a" 1 5 b c))
-
-;delete
-;1
-;(def h (AVLTreeVertex. 1 1 1 nil nil))
-;(def g (AVLTreeVertex. 3 3 1 nil nil))
-;(def f (AVLTreeVertex. 9 9 1 nil nil))
-;(def e (AVLTreeVertex. 8 8 2 nil f))
-;(def d (AVLTreeVertex. 11 11 1 nil nil))
-;(def c (AVLTreeVertex. 2 2 2 h g))
-;(def b (AVLTreeVertex. 10 10 3 e d))
-;(def a (AVLTreeVertex. 7 7 4 c b))
-;2
-;(def c (AVLTreeVertex. "c" 2 1 nil nil))
-;(def a (AVLTreeVertex. "a" 3 1 nil nil))
-;(def b (AVLTreeVertex. "b" 1 1 a c))
-
-;map
-;(map #(- % 2) (map-avl second b))
-
-;filter
-;(filter-avl #(= (second %) 2) b)
-
-;reduce
-;(def c (AVLTreeVertex. "c" "c" 1 nil nil))
-;(def a (AVLTreeVertex. "a" "a" 1 nil nil))
-;(def b (AVLTreeVertex. "b" "b" 2 a c))
-;(reduce-avl #(str %1 (second %2)) "-" b :left)
-;(reduce-avl #(str %1 (second %2)) "-" b :right)
-
-
-;merge number
-;(def c (AVLTreeVertex. 2 2 1 nil nil))
-;(def a (AVLTreeVertex. 3 3 1 nil nil))
-;(def b (AVLTreeVertex. -1 4 2 a c))
-;
-;(def f (AVLTreeVertex. 1 1 1 nil nil))
-;(def e (AVLTreeVertex. -1 -1 1 nil nil))
-;(def d (AVLTreeVertex. 0 0 2 e f))
-;merge str
-;(def c (AVLTreeVertex. 2 "c" 1 nil nil))
-;(def a (AVLTreeVertex. 3 "a" 1 nil nil))
-;(def b (AVLTreeVertex. -1 "b" 2 a c))
-;
-;(def f (AVLTreeVertex. 1 "f" 1 nil nil))
-;(def e (AVLTreeVertex. -1 "e" 1 nil nil))
-;(def d (AVLTreeVertex. 0 "d" 2 e f))
